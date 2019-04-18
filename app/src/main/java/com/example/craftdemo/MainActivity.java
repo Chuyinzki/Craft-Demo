e13d16f9bf5e09262a3599d026819d4120e6d713
Jesus Villegas <jesus_villegas@intuit.com>
2019-04-18 15:54:20 -0700

    Rough outline of what we need for app

--- app/src/main/java/com/example/craftdemo/MainActivity.java
+++ app/src/main/java/com/example/craftdemo/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //TODO: Disable rotation of app
+        //TODO: Add pull down to refresh
+        //TODO: Add logic to parse json into objects (Can stay as JSON objects if possible)
+        //TODO: Add RecyclerView to display the repositories
+        //TODO: Add loading animation to avoid clicking stale objects
+        //TODO: Add Text to show if no objects were retrieved/or there was an error to try again.
+        //TODO: Objects will all be gathered here and passed to other activities through intents for them to display.
     }
 }
